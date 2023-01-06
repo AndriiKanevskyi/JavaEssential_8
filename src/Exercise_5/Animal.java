418b301f3fba933910ad835471ffebc5973a2f62
AndriiKanevskyi <68244465+AndriiKanevskyi@users.noreply.github.com>
2023-01-06 18:20:10 +0100

    DZ 8 urok

---
+++ src/Exercise_5/Animal.java
@@ -0,0 +1,62 @@
+package Exercise_5;
+
+import java.util.Objects;
+
+public class Animal {
+    private String name;
+    private int age;
+    private boolean tail;
+
+    public Animal(String name, int age, boolean tail) {
+        this.name = name;
+        this.age = age;
+        this.tail = tail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isTail() {
+        return tail;
+    }
+
+    public void setTail(boolean tail) {
+        this.tail = tail;
+    }
+
+    @Override
+    public String toString() {
+        return "Name=" + name + "\nage=" + age +
+                "\ntail=" + tail ;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return age == animal.age && tail == animal.tail && name.equals(animal.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, tail);
+    }
+
+    public static void main(String[] args) {
+        Animal cat = new Animal("Vaska",3,true);
+        System.out.println(cat.toString());
+    }
+}
